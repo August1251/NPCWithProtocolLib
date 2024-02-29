fc83b2895dfbf191ecb9a4943a239facec45a65a
Maksim Novikov <mn939632@gmail.com>
2024-02-29 18:15:01 +0300

    The project was created.

---
+++ src/main/java/org/novikov/protocolliblearn/Protocolliblearn.java
@@ -0,0 +1,22 @@
+package org.novikov.protocolliblearn;
+
+import com.comphenix.protocol.ProtocolLibrary;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.UUID;
+
+public final class Protocolliblearn extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
